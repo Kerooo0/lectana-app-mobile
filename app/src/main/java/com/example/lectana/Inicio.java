57a38ca3b18529484966845890b9f576002fd6e1
Kerooo0 <fernando.blanco@davinci.edu.ar>
2025-09-11 18:49:51 -0300

    primer avance

---
+++ app/src/main/java/com/example/lectana/Inicio.java
@@ -0,0 +1,34 @@
+package com.example.lectana;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Inicio extends AppCompatActivity {
+
+    Button boton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_inicio);
+
+        boton = findViewById(R.id.boton_inicio);
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Inicio.this, Login.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
