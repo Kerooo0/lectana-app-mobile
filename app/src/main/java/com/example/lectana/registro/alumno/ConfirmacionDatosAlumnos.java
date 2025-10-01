1cb9401eb72701fe5ed1cdd46d23dd274822ae62
att124 <daniel.fernandez@davinci.edu.ar>
2025-10-01 11:21:13 -0300

    Layout de confirmación de datos
    Faltan las validaciones entre los distintas pantallas y el pasaje de datos

--- app/src/main/java/com/example/lectana/registro/alumno/ConfirmacionDatosAlumnos.java
+++ app/src/main/java/com/example/lectana/registro/alumno/ConfirmacionDatosAlumnos.java
@@ -9,7 +9,6 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -17,36 +16,32 @@ import android.widget.TextView;
 import com.example.lectana.R;
 
 
-public class DatosAccesoAlumno extends Fragment {
+public class ConfirmacionDatosAlumnos extends Fragment {
 
+    public ConfirmacionDatosAlumnos() {
 
-    public DatosAccesoAlumno() {
-        // Required empty public constructor
     }
-
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View vista = inflater.inflate(R.layout.fragment_datos_acceso_alumno, container, false);
+        View vista = inflater.inflate(R.layout.fragment_confirmacion_datos_alumnos, container, false);
 
         ProgressBar barraDeProgreso = vista.findViewById(R.id.barraProgreso);
-        barraDeProgreso.setProgress(2);
+        barraDeProgreso.setProgress(3);
 
         TextView barraProgresoPaso = vista.findViewById(R.id.barraProgresoPaso);
-        barraProgresoPaso.setText(getText(R.string.pasoDos));
+        barraProgresoPaso.setText(getText(R.string.pasoTres));
 
         ImageView volver = vista.findViewById(R.id.flechaVolverRegistro);
-
-        Button siguiente = vista.findViewById(R.id.boton_registrarse);
 
         volver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Fragment volver = new DatosBasicosAlumno();
+                Fragment volver = new DatosAccesoAlumno();
                 FragmentManager fragmentManager = getParentFragmentManager();
                 FragmentTransaction cambioDeFragment = fragmentManager.beginTransaction();
 
@@ -57,22 +52,7 @@ public class DatosAccesoAlumno extends Fragment {
             }
         });
 
-        siguiente.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Fragment siguiente = new ConfirmacionDatosAlumnos();
-
-                FragmentManager fragmentManager = getParentFragmentManager();
-
-                FragmentTransaction cambioDeFragment = fragmentManager.beginTransaction();
-
-                cambioDeFragment.replace(R.id.frameLayout, siguiente);
-
-                cambioDeFragment.commit();
-            }
-        });
-
         return vista;
+
     }
 }
--- app/src/main/java/com/example/lectana/registro/alumno/DatosBasicosAlumno.java
+++ app/src/main/java/com/example/lectana/registro/alumno/DatosBasicosAlumno.java
@@ -9,14 +9,21 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.lectana.R;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
 
 
 public class DatosBasicosAlumno extends Fragment {
@@ -44,6 +51,10 @@ public class DatosBasicosAlumno extends Fragment {
         nombre = vista.findViewById(R.id.editTextNombreAlumno);
 
         edad = vista.findViewById(R.id.editTextEdadAlumno);
+
+        Spinner spinnerPais = vista.findViewById(R.id.spinnerPais);
+
+        listaPaises(spinnerPais);
 
         FlechaVolver.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,6 +89,27 @@ public class DatosBasicosAlumno extends Fragment {
 
 
         return vista;
+    }
+
+    private void listaPaises(Spinner spinner) {
+        String[] paises = Locale.getISOCountries();
+        List<String> nombrePaises = new ArrayList<>();
+
+        for (String codigoPais : paises) {
+            Locale locale = new Locale("", codigoPais);
+            String nombrePais = locale.getDisplayCountry(new Locale("es", "ES"));
+            nombrePaises.add(nombrePais);
+        }
+
+        Collections.sort(nombrePaises);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                requireContext(),
+                android.R.layout.simple_spinner_item,
+                nombrePaises
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 
 
